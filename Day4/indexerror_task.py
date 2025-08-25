3bc479f7484d55f78982cb9723e0af2638189321
cf-cloud89 <udegbunamfrank89@gmail.com>
2025-08-25 20:18:38 +0100

    Practised with more examples

--- Day4/indexerror_task.py
+++ Day4/indexerror_task.py
@@ -1,3 +1,5 @@
+# IndexError Task
+
 states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
                      "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island",
                      "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
@@ -6,4 +8,21 @@ states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Conne
                      "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
                      "New Mexico", "Arizona", "Alaska", "Hawaii"]
 
-print(states_of_america)
+print(states_of_america[45])
+
+
+
+fruits = ["Oranges", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Tomatoes", "Brocoli", "Cabbage", "Lettuce"]
+
+dirty_dozen = [fruits, vegetables]
+
+# print(dirty_dozen)
+
+# print(dirty_dozen[1][1])
+
+print(dirty_dozen[0])
+# print(dirty_dozen[1])
+
+print(dirty_dozen[1][2])
+# print(dirty_dozen[1][3])
